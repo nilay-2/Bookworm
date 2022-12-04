49f3fce346985a4d6f2faaf005a638943a609cfc
Nilay Pophalkar <nilaypophalkar@gmail.com>
2022-12-04 13:47:56 +0530

    added email verification

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,24 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-module.exports = async(t,usr)=>{
-    try {
-        var transporter = nodemailer.createTransport({
-            host: "smtp-relay.sendinblue.com",
-            port: 587,
-            auth: {
-              user: "noreply.bookworm.mail@gmail.com",
-              pass: "mpKqLvZUJSCBgYIH"
-            }
-          });
+module.exports = async (t, usr) => {
+  try {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      port: 587,
+      auth: {
+        user: "nilaypophalkarvesit@gmail.com",
+        pass: "vragplwbirmbckmi",
+      },
+    });
 
-          var mailOptions = {
-            from: 'noreply.bookworm.mail@gmail.com',
-            to: usr.email,
-            subject: 'Verification For BookWorm',
-            // html: `<h1>Click On The Link Below To Verify Your Email For BookWorm</h1><br>
-            // http://localhost:3000/verify/${t.token}`
+    var mailOptions = {
+      from: "nilaypophalkarvesit@gmail.com",
+      to: usr.email,
+      subject: "Verification For BookWorm",
+      // html: `<h1>Click On The Link Below To Verify Your Email For BookWorm</h1><br>
+      // http://localhost:3000/verify/${t.token}`
 
-            html: `<!DOCTYPE html>
+      html: `<!DOCTYPE html>
             <html>
             
             <head>
@@ -208,13 +208,13 @@ module.exports = async(t,usr)=>{
                 </table>
             </body>
             
-            </html>`
-          }
+            </html>`,
+    };
 
-        await  transporter.sendMail(mailOptions);
-        return {status: "ok"};
-    } catch (error) {
-      console.log(error)
-        return {error: "error"};
-    }
-}
+    await transporter.sendMail(mailOptions);
+    return { status: "ok" };
+  } catch (error) {
+    console.log(error);
+    return { error: "error" };
+  }
+};
